31f78141e34cc27f76faab3c4cc280a2e7587d6f
DESKTOP-KNHS1LI\sebas <sebastianrosspower@gmail.com>
2024-01-25 21:56:32 +1100

    Chapter 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World"
+    user = {"username": "Miguel"}
+    usr_tasks = [
+        {
+            "task_title": "Clean bathroom",
+            "task_desc": "One person do mirrors and bench and the other does toilet and bath",
+            "due_date": "25/01/2024(3 days)",
+            "helpers": "Gustavo",
+        },
+        {
+            "task_title": "Clean windows",
+            "due_date": "27/01/2024 - 29/01/2024",
+        },
+    ]
+    return render_template("index.html", title="Home", user=user, usr_tasks=usr_tasks)
